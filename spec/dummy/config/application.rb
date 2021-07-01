5a4cf4b679f815161a37f6e84713e574a984f100
Thomas von Deyen <thomas@vondeyen.com>
2021-07-01 22:26:22 +0200

    Update dummy app to Rails 5.0

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -1,4 +1,4 @@
-require File.expand_path('../boot', __FILE__)
+require_relative "boot"
 
 # Pick the frameworks you want:
 require "active_record/railtie"
@@ -8,6 +8,8 @@ require "action_view/railtie"
 require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 require "alchemy-pg_search"
 
@@ -25,9 +27,5 @@ module Dummy
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :de
     I18n.enforce_available_locales = false
-
-    # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
   end
 end
-
