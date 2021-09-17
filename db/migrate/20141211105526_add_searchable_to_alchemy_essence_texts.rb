59dd29ab7df894568c27149789d3392a83fe46f0
DHIRAJ MISHRA <mdhiraj01@gmail.com>
2021-09-17 20:59:21 +0530

    Add version to migration to adhere to RAILS-5.1

--- db/migrate/20141211105526_add_searchable_to_alchemy_essence_texts.rb
+++ db/migrate/20141211105526_add_searchable_to_alchemy_essence_texts.rb
@@ -1,4 +1,4 @@
-class AddSearchableToAlchemyEssenceTexts < ActiveRecord::Migration
+class AddSearchableToAlchemyEssenceTexts < ActiveRecord::Migration[5.1]
   def change
     add_column :alchemy_essence_texts, :searchable, :boolean, default: true
   end
--- db/migrate/20141211110126_add_searchable_to_alchemy_essence_pictures.rb
+++ db/migrate/20141211110126_add_searchable_to_alchemy_essence_pictures.rb
@@ -1,4 +1,4 @@
-class AddSearchableToAlchemyEssencePictures < ActiveRecord::Migration
+class AddSearchableToAlchemyEssencePictures < ActiveRecord::Migration[5.1]
   def change
     add_column :alchemy_essence_pictures, :searchable, :boolean, default: true
   end
