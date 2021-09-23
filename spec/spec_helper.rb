9dd816da0e56050debb402775de34684e38adcdd
Thomas von Deyen <thomas@vondeyen.com>
2021-09-23 13:07:50 +0200

    Do not maintain schema in specs
    This does not work in a engine

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,13 +1,9 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
-ENV["RAILS_ENV"] ||= 'test'
+ENV["RAILS_ENV"] ||= "test"
 require File.expand_path("../dummy/config/environment", __FILE__)
-require 'rspec/rails'
-require 'factory_girl'
-require 'alchemy/test_support/factories'
-
-# Checks for pending migrations before tests are run.
-# If you are not using ActiveRecord, you can remove this line.
-ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
+require "rspec/rails"
+require "factory_girl"
+require "alchemy/test_support/factories"
 
 RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
