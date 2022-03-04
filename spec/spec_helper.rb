4e6b121a839b29c3f9d5ab1a0acc85605460739a
Thomas von Deyen <thomas@vondeyen.com>
2022-03-04 17:50:59 +0100

    Use factory_bot

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,11 +2,11 @@
 ENV["RAILS_ENV"] ||= "test"
 require File.expand_path("../dummy/config/environment", __FILE__)
 require "rspec/rails"
-require "factory_girl"
+require "factory_bot"
 require "alchemy/test_support/factories"
 
 RSpec.configure do |config|
-  config.include FactoryGirl::Syntax::Methods
+  config.include FactoryBot::Syntax::Methods
   config.infer_spec_type_from_file_location!
   config.use_transactional_fixtures = true
   config.infer_base_class_for_anonymous_controllers = false
