c1362b6d2f49b8d911cdf187e1297bd7188be979
Sascha Karnatz <sascha@blish.cloud>
2023-12-19 14:10:50 +0100

    Make migrations more error tolerant
    Update the migrations to Rails 6.1 and use the if_not_exists - option to make the migrations more robust. Some fields are now available in AlchemyCMS, which would break the migration on new installations.

--- db/migrate/20141211105526_add_searchable_to_alchemy_essence_texts.rb
+++ db/migrate/20141211105526_add_searchable_to_alchemy_essence_texts.rb
@@ -1,5 +1,5 @@
-class AddSearchableToAlchemyEssenceTexts < ActiveRecord::Migration[5.1]
+class AddSearchableToAlchemyEssenceTexts < ActiveRecord::Migration[6.1]
   def change
-    add_column :alchemy_essence_texts, :searchable, :boolean, default: true
+    add_column :alchemy_essence_texts, :searchable, :boolean, default: true, if_not_exists: true
   end
 end
--- db/migrate/20220826125413_add_page_id_column_to_pg_search_documents.rb
+++ db/migrate/20220826125413_add_page_id_column_to_pg_search_documents.rb
@@ -1,5 +1,7 @@
 class AddPageIdColumnToPgSearchDocuments < ActiveRecord::Migration[6.0]
   def change
-    add_reference :pg_search_documents, :page, index: true
+    unless column_exists? :pg_search_documents, :page
+      add_reference :pg_search_documents, :page, index: true
+    end
   end
 end
