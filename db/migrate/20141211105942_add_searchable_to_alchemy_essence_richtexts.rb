6942578878779205d521d54e26f6ea2ae12ca337
Sascha Karnatz <sascha@blish.cloud>
2024-10-04 10:20:17 +0200

    Remove old migrations
    Remove older migrations that were necessary for an older version of Alchemy. On fresh installation, these migration are causing troubles, because the altered tables are not available anymore for Alchemy >= 7.x.

--- db/migrate/20141211105942_add_searchable_to_alchemy_essence_richtexts.rb
+++
@@ -1,5 +0,0 @@
-class AddSearchableToAlchemyEssenceRichtexts < ActiveRecord::Migration[6.1]
-  def change
-    add_column :alchemy_essence_richtexts, :searchable, :boolean, default: true, if_not_exists: true
-  end
-end
