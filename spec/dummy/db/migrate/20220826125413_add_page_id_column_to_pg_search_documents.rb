57dba3af3501b8682a612a3e8536f57d56552254
Sascha Karnatz <sascha@blish.cloud>
2024-10-08 10:49:21 +0200

    Remove nearly all migrations from dummy app
    Migrate the dummy fresh with Alchemy 7.0 and remove nearly all migration. Ignore the migrate folder in the gitignore to store only the generated schema.rb. The only exception is the create_user migration that is a custom migration of the dummy app to have the possibility to run user specific feature specs.

--- spec/dummy/db/migrate/20220826125413_add_page_id_column_to_pg_search_documents.rb
+++
@@ -1,5 +0,0 @@
-class AddPageIdColumnToPgSearchDocuments < ActiveRecord::Migration[6.0]
-  def change
-    add_reference :pg_search_documents, :page, index: true
-  end
-end
--- spec/dummy/db/migrate/20230119143815_add_searchable_to_alchemy_pages.alchemy.rb
+++
@@ -1,8 +0,0 @@
-# This migration comes from alchemy (originally 20230119112425)
-class AddSearchableToAlchemyPages < ActiveRecord::Migration[6.0]
-  def change
-    return if column_exists?(:alchemy_pages, :searchable)
-
-    add_column :alchemy_pages, :searchable, :boolean, default: true, null: false
-  end
-end
